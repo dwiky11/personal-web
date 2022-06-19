48f0142bdfd84fccb90a5ad1ad3e5987ffe391aa
dwiky <dwiky.s98@gmail.com>
2022-06-19 15:41:06 +0700

    personal-web

--- script.js
+++ script.js
@@ -5,33 +5,6 @@ const sunIcon = document.querySelector(".toggle .bxs-sun");
 const moonIcon = document.querySelector(".toggle .bx-moon");
 const body = document.body
 
-let lastScroll = 0;
-
-window.addEventListener('scroll', () => {
-    const currentScroll = window.pageYOffset
-
-    if (currentScroll <= 0 ) {
-        body.classList.remove("scroll-up")
-    }
-
-    if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-        body.classList.remove("scroll-up")
-        body.classList.add("scroll-down")
-    }
-
-    if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
-        body.classList.remove("scroll-down")
-        body.classList.add("scroll-up")
-    }
-
-
-
-    lastScroll = currentScroll;
-
-    
-});
-
-
 menu.addEventListener('click',() => {
     nav.classList.toggle('slide');
 });
